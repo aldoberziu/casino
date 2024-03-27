ff58671b2177431c7f46a68b01b5bee4f8967a2e
Aldo Berziu <berziu2004@gmail.com>
2024-03-27 01:38:41 +0100

    new updates

--- Constants.js
+++ Constants.js
@@ -1,14 +1,28 @@
 export const works = [
-  'i hape vorrin ex-it. qe ke baba {} lek',
-  'na {} lek kot se mdo rika',
-  "tu punu si gjinekolog, klienti t'dha karin ene {} lek",
-  "{} lek meqe sot nejte ndivan si lord"
-]
+  "Ju fute punes si turku sumes. Ti gzosh {}🥮.",
+  "I hape vorrin ex-it. Qe ke baba {}🥮.",
+  "Na {}🥮 kot se mdo rika mu.",
+  "Tu punu si gjinekolog, klienti tdha karin ene {}🥮.",
+  "{}🥮 meqe sot parazitove ndivan si lord.",
+  "U kpute nbyth me aq mo. {}🥮 boll i ke.",
+  "Brravo, qe dora, kompllimente. {}🥮 per ty.",
+];
 export const niceSluts = [
-  'hape nje varr. urime ju fituat {}',
-  'na {} lek kot se mdo rika'
-]
+  "Oooooo cm knoqe. Ti gzosh kto {}🥮.",
+  "Koka nlum lodra nsum. I meritove {}🥮.",
+  "E thirre klientin President. Tka shperbly me {}🥮.",
+  "Ja le virgjerin kismetit, ja qive ropt muhabetit. {}🥮",
+  "Plak plak pidhi flak! {}🥮 ndor."
+];
 export const badSluts = [
-  'hape nje varr. urime ju fituat {}',
-  'na {} lek kot se mdo rika'
-]
+  "Se qan fare gojoren. Tkan denu me {}🥮 gjob.",
+  "U anku klienti e kishe shum t gjer. -{}🥮.",
+];
+export const niceCrimes = [
+  "I qive ropt ma t bukrit. {}🥮",
+  "I vodhe 2 thasa raiffeisenit. I punofsh me shnet {}🥮 (fsh raiffeisenin)",
+];
+export const badCrimes = [
+  "Ashu bohet krimi mer kar? U kape dhe u gjobite me {}🥮.",
+  "T'zu shteti tu vjedh lepirse imilekobur. {}🥮 gjoba.",
+];
--- components/Home/index.js
+++ components/Home/index.js
@@ -1,8 +1,8 @@
 import styles from "./Home.module.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { SendMessageDM } from "@/public";
 import Image from "next/image";
-import { works } from "@/Constants";
+import { works, niceSluts, badSluts } from "@/Constants";
 
 const Home = () => {
   const sendBtn = useRef();
@@ -11,53 +11,78 @@ const Home = () => {
   const [displayMessage, setDisplayMessage] = useState(true);
   const [bankBalance, setBankBalance] = useState(0);
   const [cashBalance, setCashBalance] = useState(0);
+  const [totalBalance, setTotalBalance] = useState(0);
 
   const getRandomNumber = (min, max) => {
     const randomDecimal = Math.random();
     const randomNumber = Math.floor(randomDecimal * (max - min + 1)) + min;
     return randomNumber;
   };
-  const getRandomWork = (works) => {
-    const randomIndex = Math.floor(Math.random() * works.length);
-    return works[randomIndex];
+  const getRandomMessage = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
   };
+  useEffect(() => {
+    setTotalBalance(cashBalance + bankBalance);
+  }, [bankBalance, cashBalance]);
 
-  const handleEnterPress = (e) => {
+  const handleEnter = (e) => {
     if (e.key === "Enter") {
-      switch (input) {
-        case "!work":
-          console.log("sdkjfsdkj");
-          const workMoney = getRandomNumber(1, 1000);
-          setCashBalance(cashBalance + workMoney);
-          let randomWork = getRandomWork(works);
-          const message = randomWork.replace("{}", workMoney);
-          setMessage(message);
-          break;
-        case "!bal":
-          setMessage(`Cash Balance: ${cashBalance}. Bank Balance: ${bankBalance}`);
-          break;
-      }
-      if (input.startsWith("!dep")) {
-        switch (input) {
-          case "!dep all":
-            setBankBalance(bankBalance + cashBalance);
-            setMessage(`Successfully deposited ${cashBalance} to your bank!`);
-            setCashBalance(0);
-            break;
-          case "!dep":
-            setMessage("Invalid number or arguments. Use !dep <amount>");
-          default:
-            const coins = input.replace(/\D/g, "");
-            if (coins > cashBalance) {
-              setMessage("You cannot deposit more than you have in cash!");
-            } else {
-              setBankBalance(bankBalance + parseInt(coins));
-              setMessage(`Successfully deposited ${coins} to your bank!`);
-              setCashBalance(cashBalance - coins);
-            }
-        }
-      }
       sendBtn.current.click();
+    }
+  };
+  const handleSubmit = () => {
+    switch (input) {
+      case "!work":
+        const workMoney = getRandomNumber(1, 1000);
+        setCashBalance(cashBalance + workMoney);
+        let randomWork = getRandomMessage(works);
+        const message = randomWork.replace("{}", workMoney);
+        setMessage(message);
+        break;
+      case "!slut":
+        const action = Math.random();
+        if (action > 0.5) {
+          const slutMoney = getRandomNumber(1, 1500);
+          setCashBalance(cashBalance + slutMoney);
+          let randomSlut = getRandomMessage(niceSluts);
+          const message = randomSlut.replace("{}", slutMoney);
+          setMessage(message);
+        } else {
+          const slutMoney = getRandomNumber(1, 2500);
+          setCashBalance(cashBalance - slutMoney);
+          let randomSlut = getRandomMessage(badSluts);
+          const message = randomSlut.replace("{}", slutMoney);
+          setMessage(message);
+        }
+        break;
+      case "!bal":
+        setMessage(
+          `Cash Balance: ${cashBalance}. Bank Balance: ${bankBalance}. Total Balance: ${totalBalance}`
+        );
+        break;
+    }
+    if (input.startsWith("!dep")) {
+      switch (input) {
+        case "!dep all":
+          setBankBalance(bankBalance + cashBalance);
+          setMessage(`Successfully deposited ${cashBalance} to your bank!`);
+          setCashBalance(0);
+          break;
+        case "!dep":
+          setMessage("Invalid number or arguments. Use !dep <amount>");
+        default:
+          const coins = input.replace(/\D/g, "");
+          if (coins > cashBalance) {
+            setMessage(
+              `You cannot deposit more than you have in cash! Currently you have ${cashBalance}`
+            );
+          } else {
+            setBankBalance(bankBalance + parseInt(coins));
+            setMessage(`Successfully deposited ${coins} to your bank!`);
+            setCashBalance(cashBalance - coins);
+          }
+      }
     }
   };
 
@@ -71,12 +96,13 @@ const Home = () => {
             setInput(e.target.value);
             setDisplayMessage(false);
           }}
-          onKeyDown={handleEnterPress}
+          onKeyDown={handleEnter}
         />
         <div
           className={`${styles.sendMessage} ${displayMessage ? styles.disabled : ""}`}
           ref={sendBtn}
           onClick={() => {
+            handleSubmit();
             setDisplayMessage(!displayMessage);
             setInput("");
           }}
